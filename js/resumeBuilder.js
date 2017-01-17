25147d657615aa6bba2b9c5486783087ef4e99b6
michaelhyun <michael.hyun3@gmail.com>
2017-01-16 23:36:54 -0800

    Added Header Name and Role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var headerName = HTMLheaderName.replace("%data%", "Michael Hyun");
+ var headerRole = HTMLheaderRole.replace("%data%", "Software Engineer at San Jose State University");
+
+ $("#header").append(headerName);
+ $("#header").append(headerRole);
